5a850d6e70d19a2b8f848f5f3600bcc5914f7223
villetiukuvaara <ville.tiukuvaara@gmail.com>
2019-09-11 13:28:34 -0400

    Add debug print

--- DMC.py
+++ DMC.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import gclib
+import util
 
 class DMC(object):
     def __init__(self, ip_address):
@@ -13,7 +14,41 @@ class DMC(object):
     def __del__(self):
         info = self.g.GInfo();
         self.g.GClose()
-        print('Closed connection to ' + info)  
+        print('Closed connection to ' + info)
+        
+    def send_command(self, command):
+        return self.g.GCommand(command);
+        
+    def disable_motors(self, motor_list):
+        for m in motor_list:
+            self.send_command('MO{}'.format(m));
+    
+    def enable_motors(self, motor_list):
+        for m in motor_list:
+            self.send_command('SH{}'.format(m));
+    
+    def configure(self):
+        m = self.disable_motors(['A', 'B,', 'C', 'D']);
+        util.dprint(m);
+        
+        # Set axis A,B,C,D to be stepper motors
+        m = self.send_command('MT 2,2,2,2')
+        util.dprint(m);
+        
+        # Set motor current (0=0.5A, 1=1A, 2=2A, 3=3A)
+        m = self.send_command('AG 1,1,1,1')
+        util.dprint('\"{}\"'.format(m));
+        
+        # Set holding current o be 25%,n samples after stopping
+        n = 15
+        m = self.send_command('LC -{0},-{0},-{0},-{0}'.format(n))
+        util.dprint(m);
+        
+        #self.enable_motors(['A', 'B,', 'C', 'D']);
+        #self.enable_motors(['A']);
+    
 
 if __name__ == "__main__":
-    d = DMC('134.117.39.229');
+    util.debug_messages = True
+    d = DMC('134.117.39.229')
+    #d.configure();
---
+++ util.py
@@ -0,0 +1,8 @@
+# Utility functions
+
+debug_messages = False
+
+# Debug print
+def dprint(string):
+    if debug_messages:
+        print(string)
