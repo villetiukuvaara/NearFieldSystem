ba7fac6bfba63551a540151abe0f8adb8279a774
villetiukuvaara <ville.tiukuvaara@gmail.com>
2019-10-24 13:18:00 -0400

    Suppress debug output after repeated messages

--- util.py
+++ util.py
@@ -1,8 +1,32 @@
 # Utility functions
 
+import threading
+
 debug_messages = False
+print_lock = threading.Lock()
+previous_messages = []
+suppressed_messages = []
+N_MESSAGE_IGNORE = 3 # Suppress output for messages that are repeated within the last 6 messages
 
 # Debug print
 def dprint(string):
     if debug_messages:
-        print(string, flush=True)
+        print_lock.acquire()
+        try:
+            if string in previous_messages:
+                if string not in suppressed_messages:
+                    print("Suppressing further output of \"{}\"".format(string), flush=True)
+                    suppressed_messages.append(string)
+            else:
+                print(string, flush=True)
+                try:
+                    suppressed_messages.remove(string)
+                except ValueError:
+                    pass
+            
+            previous_messages.append(string)
+            if(len(previous_messages) > N_MESSAGE_IGNORE):
+                previous_messages.pop(0)
+                
+        finally:
+            print_lock.release()
