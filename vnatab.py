c5f2924057031ea655114d5c0be1ba8a0d2d5708
villetiukuvaara <ville.tiukuvaara@gmail.com>
2020-10-20 12:20:50 -0400

    Add comments to vnatab.py

--- vnatab.py
+++ vnatab.py
@@ -1,6 +1,8 @@
-'''
-GUI tab for configuring the spatial measurement region
-'''
+"""A GUI tab for configuring the VNA
+
+Written by Ville Tiukuvaara
+"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -20,18 +22,28 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.figure import Figure
 import numpy as np
 
-SLEEP = 100
-PADDING = 5
-FREQ_DECIMALS = 2
-POWER_DECIMALS = 1
+SLEEP = 100  # How long to wait between updating widgets
+PADDING = 5  # Padding around widgets
+FREQ_DECIMALS = 2  # Decimal places for frequency values
+POWER_DECIMALS = 1  # Decimal placer for power in mdB
+# Default values for values in text entry boxes
 DEFAULT_PARAMS = "{start:.{s1}f} {stop:.{s1}f} {points:.0f} {power:.{s2}f} {averaging:.0f}".format(
                 start=vna.FREQ_MIN/1e9, stop=vna.FREQ_MAX/1e9, points=vna.POINTS_MAX, power=vna.POWER_MIN,
                 averaging=vna.AVERAGING_MIN,s1=FREQ_DECIMALS,s2=POWER_DECIMALS).split(" ")
-DEFAULT_ADDRESS = 16
+DEFAULT_ADDRESS = 16  # Default GPIB address for VNA
+
 
 class VNATab(tk.Frame):
-    
+    """GUI tab for controlling the VNA."""
+
     def __init__(self, parent, vna_obj, top):
+        """Init tab.
+
+        Args:
+            parent (tk.Widget): parent widget
+            vna_obj (VNA): VNA to use
+            top (NearFieldGUI): the object in charge
+        """
         self.top = top
         self.gui_ready = False
         self.disable_widgets = False
@@ -39,48 +51,46 @@ class VNATab(tk.Frame):
         self.vna = vna_obj
         self.pack()
         self.make_widgets()                      # attach widgets to self
-        self.connect_button.focus()
-        self.cal_step_done = False
-        self.next_cal_step = None
-        
+
     def make_widgets(self):
-        # Label frame for starting calibration
+        """Sets up the widgets."""
+        # Group of widgets on left side for setting up VNA connection
         left_group = tk.Frame(self)
         left_group.pack(side=tk.LEFT)
         calibrate_group = tk.LabelFrame(left_group, text="Calibration")
         calibrate_group.pack(side=tk.TOP,fill=tk.BOTH,padx=PADDING,pady=PADDING,ipadx=PADDING,ipady=PADDING)
         gpib_group = tk.Frame(calibrate_group)
         gpib_group.pack(side=tk.TOP)
-        
+
         tk.Label(gpib_group, text="GPIB address: GPIB0::").pack(side=tk.LEFT)
         self.gpib_string = tk.StringVar()
-        
+
         self.gpib_entry = tk.Entry(gpib_group, textvariable=self.gpib_string, validate="key",
                                    validatecommand=(self.register(self.validate_num), "%P", False), width=3)
         self.gpib_entry.pack(side=tk.LEFT)
         self.gpib_string.set("{}".format(DEFAULT_ADDRESS))
         tk.Label(gpib_group, text="::INSTR").pack(side=tk.LEFT)
-        
+
         cal_btn_group = tk.Frame(calibrate_group)
         cal_btn_group.pack(side=tk.TOP)
         self.connect_button = tk.Button(cal_btn_group, text="Connect",command=lambda: self.connect_btn_callback(True),width=15)
         self.connect_button.grid(row=1,column=1,padx=PADDING,pady=PADDING)
         self.disconnect_button = tk.Button(cal_btn_group, text="Disconnect",command=lambda: self.connect_btn_callback(False),width=15)
         self.disconnect_button.grid(row=1,column=2,padx=PADDING,pady=PADDING)
-    
+
         self.calibration_label = tk.Label(calibrate_group)
         self.calibration_label.pack()
 
-        # Label frame for configuring measurement
+        # Frame for configuring measurement
         meas_group = tk.Frame(left_group)
         meas_group.pack(side=tk.TOP,fill=tk.BOTH)
         config_meas_group = tk.LabelFrame(meas_group, text="Configure Measurement");
         config_meas_group.pack(side=tk.TOP,fill=tk.BOTH,expand=tk.YES,padx=PADDING,pady=PADDING,ipadx=PADDING,ipady=PADDING)
-        
-        # Desired S-parameters
+
+        # Allow user to enter desired S-parameters as checkboxes
         self.sparams = {sp : tk.IntVar() for sp in vna.SParam}
         self.sp_entries = []
-        
+
         n = 0
         for k,v in self.sparams.items():
             cb = tk.Checkbutton(config_meas_group, text=k.value, variable=v)
@@ -89,24 +99,22 @@ class VNATab(tk.Frame):
             self.sp_entries.append(cb)
             n += 1
         n += 2
-            
+
         # Labels for start, stop, step rows
         tk.Label(config_meas_group,text="Start (GHz)").grid(row=n,column=1,padx=PADDING,pady=PADDING,sticky=tk.E)
         tk.Label(config_meas_group,text="Stop (GHz)").grid(row=n+1,column=1,padx=PADDING,pady=PADDING,sticky=tk.E)
         tk.Label(config_meas_group,text="Number of points").grid(row=n+2,column=1,padx=PADDING,pady=PADDING,sticky=tk.E)
         tk.Label(config_meas_group,text="Power (dB)").grid(row=n+3,column=1,padx=PADDING,pady=PADDING,sticky=tk.E)
         tk.Label(config_meas_group,text="Averaging factor").grid(row=n+4,column=1,padx=PADDING,pady=PADDING,sticky=tk.E)
-        
+
         self.entry_strings = {}
         self.entries = []
         self.points = None
         self.step_labels = []
         validation_decimals = [True, True, False]
-        
+
+        # Set up entry boxes for frequency sweep paremeters
         for i,pos in enumerate(['start','stop','points','power','averaging']):
-            
-#            self.entry_strings[i].set(
-#                    format_str[i].format(MotionTab.DEFAULT_VALS[ax][pos_n]))
             if pos == 'points':
                 self.points = tk.ttk.Combobox(config_meas_group, values=vna.POINTS, width=5)
                 self.entries.append(self.points)
@@ -122,18 +130,24 @@ class VNATab(tk.Frame):
                                              width=7, validatecommand=(self.register(self.validate_num), "%P", True)))
                 self.entry_strings[pos].set(DEFAULT_PARAMS[i])
             self.entries[i].grid(row=i+n,column=2,padx=PADDING,pady=PADDING)
-        
+
+        # Button for taking a sample measurement
         self.measure_btn = tk.Button(left_group,text="Take measurement",command=self.measure_btn_callback)
         self.measure_btn.pack(side=tk.TOP)
-        
+
         self.measurement_plot = MeasurementPlot(self,"Title")
         self.measurement_plot.pack(side=tk.LEFT,fill=tk.BOTH)
-        
+
+        # Update the widgets when the tab becomes visible
         self.bind('<Visibility>', lambda e: self.update_widgets())
-            
+
         self.update_widgets()
-    
+
     def get_sweep_params(self):
+        """Returns the FreqSweepParams that the user has selected.
+
+        If the selected params are invalid, returns None.
+        """
         try:
             start = float(self.entry_strings['start'].get())*1e9
             stop = float(self.entry_strings['stop'].get())*1e9
@@ -141,21 +155,29 @@ class VNATab(tk.Frame):
             power = float(self.entry_strings['power'].get())
             averaging = int(self.entry_strings['averaging'].get())
             params = []
-            
+
             for sp,val in self.sparams.items():
                 if val.get():
                     params.append(sp)
-                    
+
         except ValueError:
             return None
-        
+
         try:
             return vna.FreqSweepParams(start, stop, points, power, averaging, params)
         except AssertionError:
             return None
-    
+
     def validate_num(self, P, decimals):
+        """Validates if the given number is valid (returns True/False)
+
+        Args:
+            P (str): string to be validated
+            decimals (str): "True" if the number can have decimals and is a
+            frequency; otherwise, it must be an integer.
+        """
         if decimals == "True":
+            # Match frequency
             m = re.match("^-?([0-9]*)(\.?[0-9]*)?$", P)
             try:
                 if m is None or len(m.group(1)) > 2 or len(m.group(2)) > FREQ_DECIMALS + 1:
@@ -163,6 +185,7 @@ class VNATab(tk.Frame):
             except ValueError:
                 return False
         else:
+            # Mattch int less than 9999
             m = re.match("^[0-9]*$", P)
             try:
                 if m is None or float(m.group(0)) > 9999:
@@ -171,8 +194,13 @@ class VNATab(tk.Frame):
                 if len(m.group(0)) is not 0:
                     return False
         return True
-    
+
     def connect_btn_callback(self, connect):
+        """Callback for when the user requests to connect/disconnect to VNA.
+
+        Args:
+            connect (bool): connect or disconnect?
+        """
         if connect:
             try:
                 address = int(self.gpib_string.get())
@@ -183,35 +211,44 @@ class VNATab(tk.Frame):
         else:
             self.vna.disconnect()
             self.update_widgets()
-    
+
     def connect_task(self, address):
-        self.config(cursor="wait")
+        """Performs a connection over GPIB. This is blocking!
+
+        Args:
+            address (str): GBIB address
+        """
+        self.config(cursor="wait")  # Show busy cursor
         self.vna.connect(address)
         if not self.vna.connected:
             tk.messagebox.showerror(title="VNA Error",message="Could not connect to VNA")
-        self.config(cursor="")
-        
+        self.config(cursor="")  # Show normal cursor
+
         self.update_widgets()
-    
+
     def measure_btn_callback(self):
+        """Callback to perform start sample measurement."""
         p = self.get_sweep_params()
         if p is None:
             tk.messagebox.showerror(message="Please check sweep parameters.")
             return
-            
+
+        # Check that sweep params are valid, or show error message
         msgs = p.validation_messages(check_sparams=True)
         if msgs is not None:
             tk.messagebox.showerror(message="Please fix sweep parameters.\n\n" + '\n'.join(msgs))
         else:
             threading.Thread(target=lambda: self.measure_task(p)).start()
-    
+
     def measure_task(self, params):
-        self.config(cursor="wait")
+        """Performs a sample measurement."""
+        self.config(cursor="wait")  # Show busy cursor
         data = self.vna.measure(params)
         self.measurement_plot.set_data(data)
         self.config(cursor="")
-    
+
     def enable_entries(self, enable):
+        """Enables/disables the text entry boxes."""
         if enable:
             val = tk.NORMAL
         else:
@@ -221,12 +258,17 @@ class VNATab(tk.Frame):
         for e in self.sp_entries:
             e.config(state=val)
         self.points.config(state=val)
-    
+
     def enable_widgets(self, enable=True):
+        """Enables/disables the widgets."""
         self.disable_widgets = not enable
         self.update_widgets()
-        
+
     def update_widgets(self):
+        """Updates widgets.
+
+        This is blocking, so best to call on a background thread.
+        """
         if self.disable_widgets:
             self.connect_button.config(state=tk.DISABLED)
             self.disconnect_button.config(state=tk.DISABLED)
@@ -234,7 +276,7 @@ class VNATab(tk.Frame):
             self.measure_btn.config(state=tk.DISABLED)
             self.enable_entries(False)
         elif not self.vna.connected:
-            self.calibration_label.config(text="Not connected to VNA", fg="red",height=5)
+            self.calibration_label.config(text="Not connected to VNA", fg="red", height=5)
             self.connect_button.config(state=tk.NORMAL)
             self.disconnect_button.config(state=tk.DISABLED)
             self.gpib_entry.config(state=tk.NORMAL)
@@ -242,14 +284,14 @@ class VNATab(tk.Frame):
             self.measurement_plot.set_data(None)
             self.enable_entries(False)
         elif not self.vna.cal_ok:
-            self.calibration_label.config(text="No calibration detected", fg="red",height=5)
+            self.calibration_label.config(text="No calibration detected", fg="red", height=5)
             self.connect_button.config(state=tk.DISABLED)
             self.disconnect_button.config(state=tk.NORMAL)
             self.gpib_entry.config(state=tk.DISABLED)
             self.measure_btn.config(state=tk.NORMAL)
             self.measurement_plot.set_data(None)
             self.enable_entries(True)
-        else: # Connected and calibration is ok
+        else:  # Connected and calibration is ok
             ct = self.vna.cal_type
             cal_type = ""
             if ct == vna.CalType.CALIS111:
@@ -260,9 +302,9 @@ class VNATab(tk.Frame):
                 cal_type = "2-port "
             else:
                 cal_type = "unknown"
-                
+
             text = "{} calibration detected".format(cal_type)
-            
+
             self.calibration_label.config(text=text, fg="black")
             self.connect_button.config(state=tk.DISABLED)
             self.disconnect_button.config(state=tk.NORMAL)
@@ -271,25 +313,36 @@ class VNATab(tk.Frame):
             self.enable_entries(True)
 
 class MeasurementPlot(tk.Frame):
+    """A widget that displays measured S-parameters."""
+
     def __init__(self, parent, name):
+        """Basic init. Starts a background monitor thread.
+
+        Args:
+            parent (tk.Widget): parent widget
+            name (str): name of the plot
+        """
         tk.Frame.__init__(self, parent) # do superclass init
         self.name = name
         self.pack()
         self.data = None
         self.current_sparam = None
-        
+
         self.make_widgets() # attach widgets to self
         self.update_widgets()
         self.background_task()
-    
-    # data is an array of MeasData
+
     def set_data(self, data):
-        # Only update if data changed
+        """Set plot data.
+
+        Args:
+            data (list): list of MeasData
+        """
         if self.data is data:
-            return
-        
+            return  # If the data is already plotted, don't bother again
+
         self.data = data
-        
+
         if self.data is None:
             self.plot_select.config(values=[])
             self.plot_select.set('')
@@ -299,65 +352,76 @@ class MeasurementPlot(tk.Frame):
             self.plot_select.set(sp[0])
             self.current_sparam = self.data[0].sweep_params.sparams[0]
         self.update_widgets()
-        
+
     def make_widgets(self):
+        """Sets up widgets."""
         self.fig = Figure(figsize=(5, 4), dpi=100,facecolor=(.9375,.9375,.9375))
         self.ax = self.fig.add_subplot(111)
-        #self.ax.plot(t, 2 * np.sin(2 * np.pi * t))
-        
+
         title_group = tk.Frame(self)
         title_group.pack(side=tk.TOP,pady=5)
         tk.Label(title_group, text='Choose parameter to plot: ').pack(side=tk.LEFT)
+
+        # Dropdown to select which S-parameter to plot
         self.plot_select = tk.ttk.Combobox(title_group,width=8)
         self.plot_select.bind("<<ComboboxSelected>>", lambda e: self.plot_select_callback())
         self.plot_select.pack(side=tk.LEFT,padx=5)
-        
+
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)  # A tk.DrawingArea.
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        
+
         toolbar = NavigationToolbar2Tk(self.canvas, self)
         toolbar.update()
         toolbar.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-        
+
     def plot_select_callback(self):
+        """Callback when user chooses to plot new S-param."""
         self.current_sparam = vna.SParam(self.plot_select.get())
         self.update_widgets()
-        
+
     def update_widgets(self):
+        """Forces widget to update."""
         self.request_update = True
-    
+
     def background_task(self):
+        """Tasks that runs in background to take care of replotting."""
         if self.request_update:
             self.request_update = False
             self._update_widgets()
         self.after(SLEEP, self.background_task)
-    
-    # This should only be called on the tkinter thread
+
     def _update_widgets(self):
-        self.fig.clf()
+        """Private function called by background task to actually perform
+        replotting.
+
+        This is blocking."""
+        self.fig.clf()  # Clear plot
         self.ax = self.fig.add_subplot(111)
-        self.fig.subplots_adjust(top=0.97)
-        
+        self.fig.subplots_adjust(top=0.97)  # Change space at the top
+
         if self.data is None:
             self.plot_select.config(state=tk.DISABLED)
             self.canvas.draw()
             return
         else:
             self.plot_select.config(state=tk.NORMAL)
-            
+
+        # Get MeasData for the requested S-parameter
         data = next((d for d in self.data if d.sweep_params.sparams[0] == self.current_sparam), None)
-        
+
         if data == None:
             self.canvas.draw()
             return
-        
+
+        # Plot magnitude
         colour = 'tab:red'
         self.ax.plot(data.freq/1e9, data.mag, 'r-',label='Magnitude',color=colour)
         self.ax.set_xlabel('Frequency (GHz)')
         self.ax.set_ylabel('Magnitude (dB)',color=colour)
         self.ax.tick_params(axis='y', labelcolor=colour)
-        
+
+        # Plot phase
         colour = 'tab:blue'
         ax2 = self.ax.twinx()
         ax2.plot(data.freq/1e9, data.phase, label='Phase',color=colour)
@@ -365,7 +429,3 @@ class MeasurementPlot(tk.Frame):
         ax2.tick_params(axis='y', labelcolor=colour)
         self.fig.tight_layout()
         self.canvas.draw()
-    
-        
-        
-    
