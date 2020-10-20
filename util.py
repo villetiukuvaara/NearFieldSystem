95533bc8632b5a41afb19012d8f5ddaf6538a735
villetiukuvaara <ville.tiukuvaara@gmail.com>
2020-10-20 11:02:12 -0400

    Add comments to util.py

--- util.py
+++ util.py
@@ -1,21 +1,29 @@
-# Utility functions
+"""Utility functions for near field system code.
 
+Written by Ville Tiukuvaara
+"""
 import threading
 
-debug_messages = False
-print_lock = threading.Lock()
+debug_messages = False  # By default, do not print debuging info
+print_lock = threading.Lock()  # Lock to prevent concurrent printing
 previous_messages = []
 suppressed_messages = []
-N_MESSAGE_IGNORE = 100 # Suppress output for messages that are repeated within the last 6 messages
+# Suppress output for messages that are repeated within the last 100 messages
+N_MESSAGE_IGNORE = 100
 
-# Debug print
+
 def dprint(string):
+    """Prints debugging info."""
     if debug_messages:
         print_lock.acquire()
         try:
+            # If some string is repeated, stop printing it after a certain
+            # number of occurances
             if string in previous_messages:
                 if string not in suppressed_messages:
-                    print("Suppressing further output of \"{}\"".format(string), flush=True)
+                    print(
+                        'Suppressing further output of "{}"'.format(string), flush=True
+                    )
                     suppressed_messages.append(string)
             else:
                 print(string, flush=True)
@@ -23,10 +31,10 @@ def dprint(string):
                     suppressed_messages.remove(string)
                 except ValueError:
                     pass
-            
+
             previous_messages.append(string)
-            if(len(previous_messages) > N_MESSAGE_IGNORE):
+            if len(previous_messages) > N_MESSAGE_IGNORE:
                 previous_messages.pop(0)
-                
+
         finally:
             print_lock.release()
